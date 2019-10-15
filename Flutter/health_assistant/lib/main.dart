86fe5bd69f8b38ef1c85eacbab6b82ce138bcc2f
Mohammed Arshu PT <34481775+arshupt@users.noreply.github.com>
2019-10-15 13:01:42 +0530

    arshu

--- Flutter/health_assistant/lib/main.dart
+++ Flutter/health_assistant/lib/main.dart
@@ -7,20 +7,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Health Assistent',
       theme: ThemeData(
         // This is the theme of your application.
         //
         // Try running your application with "flutter run". You'll see the
         // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
+        // changing the primarySwatch below to Colors.green and then invoku
         // "hot reload" (press "r" in the console where you ran "flutter run",
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Randheer is a kozhi'),
     );
   }
 }
